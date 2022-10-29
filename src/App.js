b143af4244a71e8a1a97e6d115147c557bc4bddf
Noman <80441451+nali556@users.noreply.github.com>
2022-10-29 18:04:17 -0400

    layouts of signup and login page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import Home from './components/Home';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Notes from './components/Notes';
 import "./App.css"
 export default function App() {
   return (
    <Routes>
       <Route path="/" element={<Home/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/signup" element={<Signup/>}/>
+      <Route path="/notes" element={<Notes/>}/>
    </Routes>
   );
 }
---
+++ src/components/Signup.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Signup() {
+    return (
+        <>
+            <div className="container">
+                Create an Account
+
+                <button className="login button">Sign Up</button>                
+            </div>
+        </>
+    )
+}
