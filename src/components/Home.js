6f6cd80719153fcba74c27606b04c4badf681f6d
Noman <80441451+nali556@users.noreply.github.com>
2022-10-29 17:25:44 -0400

    Home page outline done

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,20 @@ export default function Home() {
     return (
         <>
             <div className="home-container">
-                test
+                <div className="logo">
+                    <img src="" alt="Tech Notes Logo"/>
+                </div>
+                <div className="name">
+                    TechNotes
+                </div>
+                <div className="about-us">
+                    <p>TechNotes is an online editing software that allows you to take notes and save them anywhere you want!</p>
+                    <p>Sign up now to use!</p>
+                </div>
+                <div className="button-container">
+                    <button className="login button">Log In</button>
+                    <button className="signup button">Sign Up</button>
+                </div>
             </div>
         </>
     )
