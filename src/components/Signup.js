2b20741258416631a8128fe4c23914a1ff0d62e0
numahn <noman710@protonmail.com>
2022-11-05 12:28:17 -0400

    class changed to className

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,37 +6,23 @@ export default function Signup() {
 
             <h1 className= "Header" >Sign Up</h1>
             <div className="Sign-Up-Container">
-                <form class="row g-3">
+                <form className="row g-3">
                     <div className="Sign-Up-Form">
                         <div className="name-form">
-                            <div class="col-md-4">
-                                <label for="validationDefault01" class="form-label">First name</label>
-                                <input type="text" class="form-control" placeholder="John" id="validationDefault01" required />
+                            <div className="col-md-4">
+                                <label for="validationDefault01" className="form-label">First name</label>
+                                <input type="text" className="form-control" placeholder="John" id="validationDefault01" required />
                             </div>
-                            <div class="col-md-4">
-                                <label for="validationDefault02" class="form-label">Last name</label>
-                                <input type="text" class="form-control" placeholder="Doe" id="validationDefault02" required />
-                            </div>
-                        </div>
-                        <div class="col-md-4">
-                            <label for="validationDefaultUsername" class="form-label">Username</label>
-                            <div class="input-group">
-                                <input type="text" class="form-control" placeholder='JohnDoe@gmail.com' id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required />
+                            <div className="col-md-4">
+                                <label for="validationDefault02" className="form-label">Last name</label>
+                                <input type="text" className="form-control" placeholder="Doe" id="validationDefault02" required />
                             </div>
                         </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault03" class="form-label">Password</label>
-                            <input type="text" class="form-control" id="validationDefault03" required />
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault03" class="form-label">Confirm Password</label>
-                            <input type="text" class="form-control" id="validationDefault03" required />
-                        </div>
-                        <div class="col-md-3">
-                            <label for="validationDefault05" class="form-label">D.O.B</label>
+                        <div className="col-md-3">
+                            <label for="validationDefault05" className="form-label">Date of Birth</label>
                             <div className='Date-of-Birth'>
-                                <select class="form-select" id="validationDefault05" required >
-                                    <option selected>Choose...</option>
+                                <select className="form-select" id="validationDefault05" required >
+                                    <option selected>Month</option>
                                     <option value="1">January</option>
                                     <option value="2">February</option>
                                     <option value="3">March</option>
@@ -50,20 +36,35 @@ export default function Signup() {
                                     <option value="11">November</option>
                                     <option value="12">December</option>
                                 </select>
-                                <input type="text" class="form-control" placeholder='Date' required />
-                                <input type="text" class="form-control" placeholder='Year' required />
+                                <input type="text" className="form-control" placeholder='Date' required />
+                                <input type="text" className="form-control" placeholder='Year' required />
                             </div>
                         </div>
-                        <div class="col-12">
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required />
-                                <label class="form-check-label" for="invalidCheck2">
+                        <div className="col-md-4">
+                            <label for="validationDefaultUsername" className="form-label">Username</label>
+                            <div className="input-group">
+                                <input type="text" className="form-control" placeholder='JohnDoe@gmail.com' id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required />
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <label for="validationDefault03" className="form-label">Password</label>
+                            <input type="text" className="form-control" id="validationDefault03" required />
+                        </div>
+                        <div className="col-md-3">
+                            <label for="validationDefault03" className="form-label">Confirm Password</label>
+                            <input type="text" className="form-control" id="validationDefault03" required />
+                        </div>
+                        
+                        <div className="col-12">
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" value="" id="invalidCheck2" required />
+                                <label className="form-check-label" for="invalidCheck2">
                                     Agree to terms and conditions
                                 </label>
                             </div>
                         </div>
-                        <div class="col-12">
-                            <button class="btn btn-dark mt-3" type="submit">Submit form</button>
+                        <div className="col-12">
+                            <button className="btn btn-dark mt-3" type="submit">Submit form</button>
                         </div>
                     </div>
                 </form>
